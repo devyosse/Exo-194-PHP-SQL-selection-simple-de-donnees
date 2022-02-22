9eebcfccf2b58700d7dafafc62f021da8f5eefbd
devyosse <narutoshippuden884@gmail.com>
2022-02-22 09:16:57 +0100

    exo 194

--- index.php
+++ index.php
@@ -13,3 +13,61 @@
  * 4. Faites la même chose, mais cette fois ci, triez le résultat selon la colonne ID, du plus grand au plus petit.
  * 5. Faites la même chose, mais cette fois ci en ne sélectionnant que les noms et les prénoms.
  */
+$server = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'bdd_cours';
+
+try {
+    $pdo = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $stmt = $pdo->prepare("SELECT * FROM user");
+    $state = $stmt->execute();
+
+    if($state){
+        foreach ($stmt->fetchAll() as $user){
+            echo "<strong>Utilisateur: </strong>"
+                . $user['id']
+                . "<div>" . $user['nom'] . "</div>"
+                . "<div>" . $user['prenom'] . "</div>"
+                . "<div>" . $user['rue'] . "</div>"
+                . "<div>" . $user['numero'] . "</div>"
+                . "<div>" . $user['code_postal'] . "</div>"
+                . "<div>" . $user['ville'] . "</div>"
+                . "<div>" . $user['pays'] . "</div>"
+                . "<div>" . $user['mail'] . "</div>" . "<br><hr>";
+        }
+    }
+    $stmt = $pdo->prepare("SELECT * FROM user ORDER BY id DESC ");
+    $sate = $stmt->execute();
+    if($state){
+        foreach ($stmt->fetchAll() as $user){
+            echo "<strong>Utilisateur: </strong>"
+                . $user['id']
+                . "<div>" . $user['nom'] . "</div>"
+                . "<div>" . $user['prenom'] . "</div>"
+                . "<div>" . $user['rue'] . "</div>"
+                . "<div>" . $user['numero'] . "</div>"
+                . "<div>" . $user['code_postal'] . "</div>"
+                . "<div>" . $user['ville'] . "</div>"
+                . "<div>" . $user['pays'] . "</div>"
+                . "<div>" . $user['mail'] . "</div>" . "<br><hr>";
+        }
+    }
+    $stmt = $pdo->prepare("SELECT nom, prenom FROM user");
+    $state = $stmt->execute();
+    if($state){
+        foreach ($stmt->fetchAll() as $user){
+            echo "<strong>Utilisateur: </strong>"
+                . "<div>" . $user['nom'] . "</div>"
+                . "<div>" . $user['prenom'] . "</div>"
+                ."<br><hr>";
+        }
+    }
+
+}
+catch (PDOException $e){
+    echo $e->getMessage();
+}
